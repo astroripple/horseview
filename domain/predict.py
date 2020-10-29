d07ef41569b29fc4ccba908dbdfba46596eb04c3
Yuma Iwasaki <requrd1989@gmail.com>
2020-10-29 21:54:34 +0900

    Add predict field

--- domain/predict.py
+++ domain/predict.py
@@ -5,6 +5,8 @@ class PredictData(baseobj):
     __tablename__ = "predict"
     racehorsekey = colobj(strobj, fkyobj("racehorse.racehorsekey"), primary_key=True)
     pp_icchaku = colobj(intobj)
+    pp_nichaku = colobj(intobj)
+    pp_sanchaku = colobj(intobj)
     rentai_rate = colobj(intobj)
     fukusho_rate = colobj(intobj)
     tansho_odds = colobj(intobj)
