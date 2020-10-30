1fa35d918489e8a86853f88a8aa3b7ec7476a2e1
Yuma Iwasaki <requrd1989@gmail.com>
2020-10-30 23:45:26 +0900

    Add meta data

--- sessioncontroll.py
+++ sessioncontroll.py
@@ -3,11 +3,21 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_restless import APIManager
 from flask_migrate import Migrate, MigrateCommand
 import os
+from sqlalchemy import MetaData
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///{}/jrdb.db".format(os.environ["DB"])
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-db = SQLAlchemy(app)
+
+convention = {
+    "ix": 'ix_%(column_0_label)s',
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
+metadata = MetaData(naming_convention=convention)
+db = SQLAlchemy(app, metadata=metadata)
 migrate = Migrate(app, db)
 
 strobj = db.String(80)
