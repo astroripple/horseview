53caa527b4276cc38b6799ed367c08676316055d
Yuma Iwasaki <requrd1989@gmail.com>
2022-05-12 09:26:24 +0900

    Remove unnecessary columns

--- domain/umaren_odds.py
+++ domain/umaren_odds.py
@@ -17,5 +17,4 @@ class UmarenOddsData(baseobj):
     ran_horses = colobj(intobj)
     sold_flg = colobj(intobj)
     all_odds = colobj(jsonobj)
-    all_estimated_odds = colobj(jsonobj)
     sum_of_all_bought_count = colobj(intobj)
--- domain/wide_odds.py
+++ domain/wide_odds.py
@@ -4,9 +4,7 @@ from ..sessioncontroll import (
     baseobj,
     intobj,
     colobj,
-    relobj,
     fkyobj,
-    bkrobj,
     jsonobj,
 )
 
@@ -19,5 +17,4 @@ class WideOddsData(baseobj):
     ran_horses = colobj(intobj)
     sold_flg = colobj(intobj)
     all_odds = colobj(jsonobj)
-    all_estimated_odds = colobj(jsonobj)
     sum_of_all_bought_count = colobj(intobj)
