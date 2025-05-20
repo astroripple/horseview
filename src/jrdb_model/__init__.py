31add855888cb66df0f8e76032271925083faca7
Yuma Iwasaki <requrd1989@gmail.com>
2025-05-20 13:01:48 +0900

    fix to import

--- src/jrdb_model/__init__.py
+++ src/jrdb_model/__init__.py
@@ -1,26 +1,46 @@
 # テーブル一覧をインポートする
 from .domain.bangumi import BangumiData
+from .domain.calculated_score import CalculatedScoreData
 from .domain.kaisai import KaisaiData
+from .domain.predict import PredictData
+from .domain.predict_race import PredictRaceData
 from .domain.racehorse import RacehorseData
 from .domain.returninfo import ReturninfoData
-from .domain.umaren_odds import UmarenOddsData
-from .domain.wide_odds import WideOddsData
-from .domain.wakuren_odds import WakurenOddsData
 from .domain.seiseki import SeisekiData
 from .domain.seisekirace import SeisekiRaceData
 from .domain.trainanalysis import TrainAnalysisData
 from .domain.trainoikiri import TrainOikiriData
-from .domain.predict import PredictData
-from .domain.predict_race import PredictRaceData
-from .domain.calculated_score import CalculatedScoreData
-
+from .domain.umaren_odds import UmarenOddsData
+from .domain.wakuren_odds import WakurenOddsData
+from .domain.wide_odds import WideOddsData
 from .master.horsebase import HorsebaseData
+from .master.jockey import JockeyData
 from .master.mastercode import *
 from .master.trainer import TrainerData
-from .master.jockey import JockeyData
 
 # 更新処理用のセッションオブジェクト
-from .sessioncontroll import sesobj
-
 # flask用オブジェクト
-from .sessioncontroll import app, db
+from .sessioncontroll import app, db, sesobj
+
+__all__ = [
+    "BangumiData",
+    "KaisaiData",
+    "RacehorseData",
+    "ReturninfoData",
+    "UmarenOddsData",
+    "WideOddsData",
+    "WakurenOddsData",
+    "SeisekiData",
+    "SeisekiRaceData",
+    "TrainAnalysisData",
+    "TrainOikiriData",
+    "PredictData",
+    "PredictRaceData",
+    "CalculatedScoreData",
+    "HorsebaseData",
+    "TrainerData",
+    "JockeyData",
+    "sesobj",
+    "app",
+    "db",
+]
