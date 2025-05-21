9baa3a5571aaf4f170269eed5869ec41d300e3e2
Yuma Iwasaki <requrd1989@gmail.com>
2025-05-21 19:26:15 +0900

    add comments

--- tests/test_kaisai.py
+++ tests/test_kaisai.py
@@ -1,15 +1,19 @@
 def test_kaisai():
+    # import os
+    from typing import List
+
     from sqlalchemy.orm import joinedload
 
     from jrdb_model import KaisaiData, create_app
 
+    # os.environ["DB"] = mariadb+pymysql://user:pass@host/database
     app = create_app()
     with app.app_context():
-        kaisais = (
+        kaisais: List[KaisaiData] = (
             KaisaiData.query.filter(
                 KaisaiData.ymd >= 20220101, KaisaiData.ymd <= 20220130
             )
             .options(joinedload("*"))
             .all()
         )
-        assert len(kaisais) == 1
+        assert len(kaisais) == 26
