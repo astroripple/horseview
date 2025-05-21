59f273214334b390e5ad6f9668236abe14f61b34
Yuma Iwasaki <requrd1989@gmail.com>
2025-05-21 17:32:56 +0900

    fix to use mapped

--- src/jrdb_model/domain/calculated_score.py
+++ src/jrdb_model/domain/calculated_score.py
@@ -1,8 +1,22 @@
-from ..sessioncontroll import baseobj, colobj, fkyobj, floatobj, strobj
+"""計算済みスコア."""
+
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column
+
+from ..sessioncontroll import db
 
 
-class CalculatedScoreData(baseobj):
+class CalculatedScoreData(db.Model):
+    """計算済みスコア.
+
+    Args:
+        db (_type_): _description_
+
+    """
+
     __tablename__ = "calculated_score"
-    racehorsekey = colobj(strobj, fkyobj("racehorse.racehorsekey"), primary_key=True)
-    waku_win_rate = colobj(floatobj)
-    waku_rentai_rate = colobj(floatobj)
+    racehorsekey: Mapped[str] = mapped_column(
+        ForeignKey("racehorse.racehorsekey"), primary_key=True
+    )
+    waku_win_rate: Mapped[float] = mapped_column()
+    waku_rentai_rate: Mapped[float] = mapped_column()
