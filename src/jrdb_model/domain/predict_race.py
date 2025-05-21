2498fff8af088095ae920b9b416083d31bc2fed9
Yuma Iwasaki <requrd1989@gmail.com>
2025-05-21 17:51:35 +0900

    fix to use mapped

--- src/jrdb_model/domain/predict_race.py
+++ src/jrdb_model/domain/predict_race.py
@@ -1,15 +1,23 @@
-from ..sessioncontroll import (
-    baseobj,
-    colobj,
-    fkyobj,
-    jsonobj,
-    strobj,
-)
+"""予想レースデータ."""
+
+from sqlalchemy import JSON, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column
+
+from ..sessioncontroll import db
 
 
-class PredictRaceData(baseobj):
+class PredictRaceData(db.Model):
+    """予想レースデータ.
+
+    Args:
+        db (_type_): _description_
+
+    """
+
     __tablename__ = "predict_race"
-    racekey = colobj(strobj, fkyobj("bangumi.racekey"), primary_key=True)
-    umaren = colobj(jsonobj)
-    wide = colobj(jsonobj)
-    wakuren = colobj(jsonobj)
+    racekey: Mapped[str] = mapped_column(
+        ForeignKey("bangumi.racekey"), primary_key=True
+    )
+    umaren: Mapped[str] = mapped_column(type_=JSON)
+    wide: Mapped[str] = mapped_column(type_=JSON)
+    wakuren: Mapped[str] = mapped_column(type_=JSON)
