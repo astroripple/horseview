459769322ed7904c898fedc1f76cfec7e061c48a
Yuma Iwasaki <requrd1989@gmail.com>
2025-05-21 13:37:17 +0900

    add base class

--- src/jrdb_model/sessioncontroll.py
+++ src/jrdb_model/sessioncontroll.py
@@ -6,16 +6,30 @@ from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
+from sqlalchemy.orm import DeclarativeBase, MappedAsDataclass
 
-convention = {
-    "ix": "ix_%(column_0_label)s",
-    "uq": "uq_%(table_name)s_%(column_0_name)s",
-    "ck": "ck_%(table_name)s_%(constraint_name)s",
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-    "pk": "pk_%(table_name)s",
-}
-metadata = MetaData(naming_convention=convention)
-db = SQLAlchemy(metadata=metadata)
+
+class Base(DeclarativeBase, MappedAsDataclass):
+    """公式推奨のベースクラス.
+
+    Args:
+        DeclarativeBase (_type_): _description_
+        MappedAsDataclass (_type_): _description_
+
+    """
+
+    metadata = MetaData(
+        naming_convention={
+            "ix": "ix_%(column_0_label)s",
+            "uq": "uq_%(table_name)s_%(column_0_name)s",
+            "ck": "ck_%(table_name)s_%(constraint_name)s",
+            "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+            "pk": "pk_%(table_name)s",
+        }
+    )
+
+
+db = SQLAlchemy(model_class=Base)
 migrate = Migrate()
 
 
